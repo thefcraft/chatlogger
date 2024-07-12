c35b736fff56ef0b4319abafa30ed35fd69b111b
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2024-07-12 17:01:24 +0530

    pipnameChange

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
-    name="chatlogger",
+    name="chat-logger",
     version="0.1.0",
     author="ThefCraft",
     author_email="sisodiyalaksh@gmail.com",
